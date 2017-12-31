f9be566f1e6ed7f41838bd78da3bb1de3e21f5d9
Võ Duy Thảo <duythao56@gmail.com>
2017-12-31 23:50:15 +0700

    config project restful

---
+++ JAVA-RestfulAPI/src/main/java/com/JAVARestfulAPI/JavaRestfulApiApplication.java
@@ -0,0 +1,12 @@
+package com.JAVARestfulAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRestfulApiApplication.class, args);
+	}
+}
---
+++ JAVA-RestfulAPI/src/test/java/com/JAVARestfulAPI/JavaRestfulApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.JAVARestfulAPI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaRestfulApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
